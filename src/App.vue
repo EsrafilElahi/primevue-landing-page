c7ce75996a94ebe45bb45c3d98594d964bc56153
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-15 14:46:48 +0330

    use tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
-<script setup>
-</script>
-
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <h1 class="text-[#d0c]">salam</h1>
+  </div>
 </template>
+
+<script setup>
+
+</script>
 
 <style scoped></style>
