0976a0a756bc4f7a1cdf4ded927baea17754bc7e
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-15 16:46:51 +0330

    add all components primevue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,9 +5,24 @@
     <RouterLink to="/search-rent-house">Search Rent House</RouterLink>
     <RouterLink to="/search-sold-house">Search Sold House</RouterLink>
   </div>
+
+  <div class="card">
+    <TabMenu v-model:activeIndex="active" :model="items" />
+  </div>
 </template>
 
 <script setup>
+
+import { ref } from "vue";
+
+const active = ref(0);
+
+const items = ref([
+  { label: 'Dashboard', icon: 'pi pi-home' },
+  { label: 'Transactions', icon: 'pi pi-chart-line' },
+  { label: 'Products', icon: 'pi pi-list' },
+  { label: 'Messages', icon: 'pi pi-inbox' }
+]);
 
 </script>
 
