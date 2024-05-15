148cd1c2e6405e1b0bb863d82fe86389708c3562
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-15 16:24:13 +0330

    add routerView tag

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <template>
-  <div>
-
-    <InputText placeholder="test" />
-    <span class="pi pi-user pi-spin pi-cog" style="color: #708090"></span>
-  </div>
-
-  <div class="flex flex-column md:flex-row md:justify-content-between row-gap-3">
-    <Button label="Button 1" icon="pi pi-user" severity="success"></Button>
-    <Button label="Button 2" severity="secondary" outlined></Button>
-    <Button label="Help" severity="help" outlined></Button>
-    <Button label="Success" severity="success" rounded raised outlined></Button>
-  </div>
+  <RouterView></RouterView>
 </template>
 
 <script setup>
