62c7b169ff1d57d7882734c032e8c37404e6693c
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-18 10:53:20 +0330

    adding tehran city

--- src/utils.js
+++ src/utils.js
@@ -5,6 +5,6 @@ export const axiosInstance = axios.create({
 });
 
 const RAPID_API_KEY = "917d9af744msh5bbe388a9895c71p14050djsn53926f762f6f";
-const RAPID_API_HOST = "917d9af744msh5bbe388a9895c71p14050djsn53926f762f6f";
+const RAPID_API_HOST = "realtor-search.p.rapidapi.com";
 axiosInstance.defaults.headers.common["X-Rapidapi-Key"] = RAPID_API_KEY;
 axiosInstance.defaults.headers.common["X-Rapidapi-Host"] = RAPID_API_HOST;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,12 +11,30 @@
 import { axiosInstance } from '../utils';
 import CitiesTabs from '../components/CitiesTabs.vue';
 import { useRoute } from 'vue-router'
-import { onMounted } from 'vue';
+import { onMounted, watch, watchEffect } from 'vue';
 const route = useRoute()
 
+const fetchData = async (userName) => {
+  try {
+    const res = await axiosInstance.get('https://realtor-search.p.rapidapi.com/properties/search-buy', {
+      params: {
+        location: `city:${userName || 'New York'}`
+      }
+    });
+    console.log('res :', res);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
 onMounted(() => {
-  axiosInstance.get('https://realtor-search.p.rapidapi.com/properties/search-buy')
-})
+  fetchData(route.query.userName);
+});
+
+watch(() => route.query.userName, (newUserName) => {
+  fetchData(newUserName);
+});
+
 
 </script>
 
