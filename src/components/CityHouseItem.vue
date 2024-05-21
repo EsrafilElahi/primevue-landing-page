5583d622d7574badb93ae53f4342ac1491501b00
esrafil elahi <esrafil.elahi@gmail.com>
2024-05-21 09:32:48 +0330

    add props

--- src/components/CityHouseItem.vue
+++ src/components/CityHouseItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="p-3 border rounded-lg w-[20rem]">
-    <img src="https://fakeimg.pl/300/" alt="test" class="w-full h-full" />
+    <img src={{props?.item?.photos?.at(0)?.href}} alt="test" class="w-full h-full" />
     <div class="mt-3">
       <address>address : tehran, iran</address>
     </div>
-    <div class="my-1">href: <a href="www.google.com">link</a></div>
+    <div class="my-1">href: <a href="www.google.com">www.google.com</a></div>
     <div>
       <span>link : <span>https://google.com</span></span>
     </div>
@@ -12,6 +12,7 @@
 </template>
 
 <script setup>
+const props = defineProps(['item'])
 
 </script>
 
