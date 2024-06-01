f073037f2cf092e7b5fb61c979873cd62aa0fc01
esrafil elahi <esrafil.elahi@gmail.com>
2024-06-01 17:12:52 +0330

    add button

--- src/components/PokemonItem.vue
+++ src/components/PokemonItem.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex flex-col gap-2 p-3 border rounded-lg w-[20rem]">
     <h1>{{ item.name }}</h1>
-    <a :href="item.url" class="text-blue-500">link</a>
+    <a :href="item.url" class="w-full">
+      <Button severity="info" class="w-full" label="Link">
+      </Button>
+    </a>
   </div>
 </template>
 
